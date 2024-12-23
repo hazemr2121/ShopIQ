d73ad97cc8cf248578c59103d836a0f918673ad0
el3raky <kameladel339@gmail.com>
2024-12-23 21:14:08 +0200

    created store page

--- assets/js/store.js
+++ assets/js/store.js
@@ -12,17 +12,20 @@ const firebaseConfig = {
         "https://store-ec3ce-default-rtdb.europe-west1.firebasedatabase.app/",
 };
 const app = initializeApp(firebaseConfig);
+const db = getDatabase();
+
 console.log(app);
+
+
 
 var productsData = [];
 
-const db = getDatabase();
 const productsRef = ref(db, "products");
 onValue(productsRef, (snapshot) => {
     productsData = snapshot.val();
 
     var productsContainer = document.querySelector(".store_page .products-list");
-    console.log(productsContainer);
+
     if (productsData.length === 0) {
         document.getElementById("loading").style.display = "block";
 
@@ -30,31 +33,31 @@ onValue(productsRef, (snapshot) => {
         document.querySelector(".store_page .container_content").style.display = "flex";
         document.getElementById("loading").style.display = "none";
 
-        productsData.slice(10, 19).forEach((product) => {
-            let { name, category, price, description, discountPercentage, rating, stock_quantity, reviews, brand, thumbnail } = product;
+        productsData.slice(0, 9).forEach((product) => {
+            let { name, category, price, description, discountPercentage, rating, brand, thumbnail } = product;
 
             var productCard = document.createElement("div");
             productCard.className = "product_card";
-            productCard.innerHTML = `<span class="badge">20%</span>
+            productCard.innerHTML = `<span class="badge">${discountPercentage}%</span>
                                     <span class="wish-icon">
                                         <i class="fa-regular fa-heart"></i>
                                     </span>
-                                    <img src=${product.thumbnail} alt="product img" />
+                                    <img src=${thumbnail} alt="product img" />
                                     <div class="product-content">
                                         <div style="display: flex; justify-content: space-between; align-items: center; margin-bottom: 10px; color: #db5807;">
-                                            <div class="product-category">${product.category}</div>
-                                            <div class="product-brand">${product.brand}</div>
+                                            <div class="product-category">${category}</div>
+                                            <div class="product-brand">${brand}</div>
                                         </div>
                                         <a href="" class="product_link">
-                                            <div class="product-name ">${product.name}</div>
+                                            <div class="product-name ">${name}</div>
                                         </a>
-                                        <div class="description">${product.description.substring(0, 60) + "...."}</div>
+                                        <div class="description">${description.substring(0, 60) + "...."}</div>
                                         <div class="rating">
-                                            <i class="fa-solid fa-star" style="color: #FFD43B;"></i>${product.rating}
+                                            <i class="fa-solid fa-star" style="color: #FFD43B;"></i>${rating}
                                         </div>
                                         <hr />
                                         <div style="display: flex; justify-content: space-between; align-items: center; margin-top: 10px;">
-                                            <div class="price">$ ${product.price}</div>
+                                            <div class="price">$ ${price}</div>
                                             <button class="btn add-to-cart ">
                                                 <i class="fa-solid fa-cart-plus" style="margin-right: 5px;"></i>Add to Cart
                                             </button>
@@ -62,9 +65,35 @@ onValue(productsRef, (snapshot) => {
                                     </div>`;
 
             productsContainer.appendChild(productCard);
-        });
 
+        });
     }
 
-    console.log(productsData.slice(10, 19)[0]);
+    console.log(productsData.slice(0, 9));
 });
+
+
+// get categories from firebase
+function getCategories() {
+
+    const categoriesRef = ref(db, "categories");
+    onValue(categoriesRef, (snapshot) => {
+        const categories = snapshot.val();
+        console.log(categories);
+
+        categories.forEach((category) => {
+            var categoryItem = document.createElement("div");
+            categoryItem.className = "category-item";
+            categoryItem.innerHTML = `<p>${category}</p>`;
+            document.querySelector(".filters .categories").appendChild(categoryItem);
+        })
+    });
+}
+
+getCategories();
+
+
+let categoryAccordion = document.getElementById("category-accordion");
+categoryAccordion.onclick = function () {
+    categoryAccordion.classList.toggle("active");
+}
