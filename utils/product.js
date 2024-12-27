39aceb52e29fc8652dea68274c9a98ca80af595c
el3raky <kameladel339@gmail.com>
2024-12-27 18:07:08 +0200

    implement add-to-cart & add-to-wishlist

---
+++ utils/product.js
@@ -0,0 +1,26 @@
+export async function addToCart(product_data, user_id) {
+    const response = await fetch(`http://localhost:3000/api/users/${user_id}/cart` , {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(product_data),
+    })
+
+    const data = await response.json();
+    return data;
+}
+
+
+export async function addToWishlist( product_data, user_id) {
+    const response = await fetch(`http://localhost:3000/api/users/${user_id}/wishlist`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(product_data),
+    });
+
+    const data = await response.json();
+    return data;
+}
