efc29080335a53ff8ac422753233a047ea3c9ddb
hazemr2121 <ha.ahmed8996@gmail.com>
2024-12-30 05:23:18 +0200

    wishlist and contact page

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -1,4 +1,4 @@
-console.log(localStorage.getItem("user"));
+// console.log(localStorage.getItem("user"));
 
 if (!localStorage.getItem("user")) {
   location.href = "http://127.0.0.1:5501/login&register/login.html";
